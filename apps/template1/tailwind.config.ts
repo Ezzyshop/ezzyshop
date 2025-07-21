5b57e1347b7fb4408970385c1ff41da1d66db209
Ulugbek-desktop <ulugb3k270@gmail.com>
2025-07-21 18:09:38 +0500

    Remove Tailwind CSS configuration file from template application.

--- apps/template1/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}", // <-- Add this line
-  ],
-  // ...other config options
-} satisfies Config;
