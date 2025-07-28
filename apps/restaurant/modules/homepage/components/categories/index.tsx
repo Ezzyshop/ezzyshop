8625b21869e2cbb9812464b1ef24465afca46a4a
Ulugbek-desktop <ulugb3k270@gmail.com>
2025-07-28 21:01:07 +0500

    Enhance Categories component in restaurant application by integrating localization support and dynamic category rendering. Update Uzbek translation files to include new category titles for improved internationalization. Refactor component structure for better UI presentation.

--- apps/restaurant/modules/homepage/components/categories/index.tsx
+++ apps/restaurant/modules/homepage/components/categories/index.tsx
@@ -2,16 +2,43 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { CategoriesService } from "@repo/api/services";
+import { useTranslations, useLocale } from "next-intl";
+import Image from "next/image";
 
 interface IProps {
   shopId: string;
 }
 
 export const Categories = ({ shopId }: IProps) => {
+  const language = useLocale();
+  const t = useTranslations("homepage.categories");
   const { data } = useQuery({
     queryKey: ["categories", shopId],
     queryFn: () => CategoriesService.getCategories(shopId),
   });
 
-  return <div>categories</div>;
+  return (
+    <div className="mt-10">
+      <h2 className="font-semibold text-xl px-4">{t("title")}</h2>
+      <div className="grid grid-cols-4 place-content-center gap-2 mt-3">
+        {data?.data.map((category) => (
+          <div
+            key={category._id}
+            className="flex flex-col items-center cursor-pointer"
+          >
+            <Image
+              src={category?.image || ""}
+              alt={category.name[language]}
+              width={74}
+              height={74}
+              className=" w-[74px] h-[74px] object-cover rounded-full"
+            />
+            <p className="text-center text-sm mt-2 font-medium">
+              {category.name[language]}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
