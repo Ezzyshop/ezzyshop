cd5ee17928f3c8e4cbc761a3a5cc873c3448409e
Ulugbek <ulugb3k270@gmail.com>
2025-07-31 16:52:29 +0500

    Enhance Next.js configuration in market and restaurant apps by adding @repo/hooks and @repo/api to transpilePackages. Introduce experimental optimizePackageImports for @repo/ui. Update postcss.config.mjs to include Tailwind CSS plugin for improved styling support.

--- apps/market/next.config.ts
+++ apps/market/next.config.ts
@@ -4,7 +4,16 @@ const withNextIntl = createNextIntlPlugin("./utils/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@repo/ui", "@repo/i18n", "@repo/api", "@repo/contexts"],
+  transpilePackages: [
+    "@repo/ui",
+    "@repo/i18n",
+    "@repo/contexts",
+    "@repo/api",
+    "@repo/hooks",
+  ],
+  experimental: {
+    optimizePackageImports: ["@repo/ui"],
+  },
 };
 
 export default withNextIntl(nextConfig);
