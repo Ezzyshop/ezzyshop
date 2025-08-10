5668c9b9058a4afec4786074ae4e8af17fcedf38
Ulugbek <ulugb3k270@gmail.com>
2025-08-10 10:10:26 +0500

    Update AddressSelect component to utilize translations for address label, enhancing localization support. Adjust DrawerContent layout for improved UI consistency.

--- apps/restaurant/modules/homepage/components/address-select/address-select.tsx
+++ apps/restaurant/modules/homepage/components/address-select/address-select.tsx
@@ -7,17 +7,18 @@ import {
 } from "@repo/ui/components/ui/drawer";
 import { LocationEdit } from "@repo/ui/icons";
 import { AddressSelect as SharedAddressSelect } from "@/components/address-select/address-select";
+import { useTranslations } from "next-intl";
 
 export const AddressSelect = () => {
+  const t = useTranslations("profile.address");
   return (
     <Drawer>
       <DrawerTrigger className="bg-primary/10  w-full text-primary py-2 border-y border-primary/20 text-sm font-medium flex items-center justify-center gap-2 cursor-pointer">
-        <LocationEdit className="w-4 h-4" /> <span>AddressSelect</span>
+        <LocationEdit className="w-4 h-4" /> <span>{t("addresses")}</span>
       </DrawerTrigger>
-      <DrawerContent>
-        <DrawerTitle>
-          <SharedAddressSelect />
-        </DrawerTitle>
+      <DrawerContent className="px-4 pb-4 mt-3 space-y-4">
+        <DrawerTitle className="hidden">{t("addresses")}</DrawerTitle>
+        <SharedAddressSelect />
       </DrawerContent>
     </Drawer>
   );
