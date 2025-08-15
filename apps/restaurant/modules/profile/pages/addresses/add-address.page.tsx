cf6481b4aa0de7b5ed10394586b82c33c50b48b0
Ulugbek-desktop <ulugb3k270@gmail.com>
2025-08-15 11:09:56 +0500

    Refactor address addition logic to improve user context handling: Update AddAddressPage to move user context retrieval after router initialization and enhance onSuccess mutation to await query invalidation before navigation, ensuring smoother address addition flow.

--- apps/restaurant/modules/profile/pages/addresses/add-address.page.tsx
+++ apps/restaurant/modules/profile/pages/addresses/add-address.page.tsx
@@ -4,20 +4,20 @@ import { useTranslations } from "next-intl";
 import { AddAddressForm } from "../../components/add-address/add-address-form";
 import { AddressService } from "@repo/api/services/address/address.service";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useUserContext } from "@repo/contexts/user-context/user.context";
 import { useRouter } from "nextjs-toploader/app";
 import { useParams } from "next/navigation";
+import { useUserContext } from "@repo/contexts/user-context/user.context";
 
 export const AddAddressPage = () => {
   const t = useTranslations("profile.address");
   const queryClient = useQueryClient();
-  const { user } = useUserContext();
   const router = useRouter();
   const { locale, shopId } = useParams();
+  const { user } = useUserContext();
   const { mutate: addAddress, isPending: isLoading } = useMutation({
     mutationFn: AddressService.createAddress,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
         queryKey: ["addresses", user?._id],
       });
       router.push(`/${locale}/${shopId}/profile/addresses`);
