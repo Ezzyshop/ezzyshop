cb38021ef66ee99415ab43538cd0649793915982
Ulugbek-desktop <ulugb3k270@gmail.com>
2025-08-25 04:56:03 +0500

    Enhance useTMAInit hook to integrate brand color and improve WebApp header settings: Added brand color from shop context and updated header color dynamically. Refactored useEffect hooks for better organization and clarity.

--- apps/market/hooks/use-tma-init.ts
+++ apps/market/hooks/use-tma-init.ts
@@ -3,12 +3,14 @@ import { useEffect } from "react";
 
 import { useParams, usePathname, useRouter } from "next/navigation";
 import WebApp from "@twa-dev/sdk";
+import { useShopContext } from "@/contexts/shop.context";
 
 export const useTMAInit = () => {
   const pathname = usePathname();
   const { shopId, locale } = useParams();
   const isHomePage = pathname === `/${locale}/${shopId}/home`;
   const router = useRouter();
+  const { brand_color } = useShopContext();
 
   useEffect(() => {
     if (isHomePage) {
@@ -17,9 +19,14 @@ export const useTMAInit = () => {
     WebApp.BackButton.onClick(() => {
       router.back();
     });
+  }, [isHomePage, router]);
 
+  useEffect(() => {
+    WebApp.setHeaderColor((brand_color as `#${string}`) ?? "#000000");
+  }, [brand_color]);
+
+  useEffect(() => {
     WebApp.enableClosingConfirmation();
     WebApp.disableVerticalSwipes();
-    WebApp.setHeaderColor("--primary" as `#${string}`);
-  }, [isHomePage, router]);
+  }, []);
 };
