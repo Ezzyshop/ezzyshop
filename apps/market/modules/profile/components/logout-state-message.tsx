24e5fc72b9e8a09de7e0fd755ec71744ec6710cf
Ulugbek-desktop <ulugb3k270@gmail.com>
2026-02-17 21:01:27 +0500

    Refactor LogoutStateMessage component: Simplified avatar rendering logic by conditionally displaying the logo using the Image component when available, enhancing visual presentation and maintaining fallback behavior for user initials.

--- apps/market/modules/profile/components/logout-state-message.tsx
+++ apps/market/modules/profile/components/logout-state-message.tsx
@@ -1,12 +1,9 @@
 import { useShopContext } from "@/contexts/shop.context";
 import { useUserContext } from "@repo/contexts/user-context/user.context";
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@repo/ui/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@repo/ui/components/ui/avatar";
 import { Card } from "@repo/ui/components/ui/card";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 export const LogoutStateMessage = () => {
   const { logo, name } = useShopContext();
@@ -17,10 +14,20 @@ export const LogoutStateMessage = () => {
 
   return (
     <Card className="h-[30vh] flex flex-col items-center justify-center gap-3 shadow-none border-0">
-      <Avatar className="size-16">
-        <AvatarImage src={logo ?? undefined} />
-        <AvatarFallback>{name?.charAt(0)}</AvatarFallback>
-      </Avatar>
+      {!logo ? (
+        <Avatar className="size-16">
+          <AvatarFallback>{name?.charAt(0)}</AvatarFallback>
+        </Avatar>
+      ) : (
+        <Image
+          src={logo}
+          alt="logo"
+          className="rounded-xl object-cover max-w-full w-fit aspect-square"
+          height={96}
+          width={96}
+          fetchPriority="high"
+        />
+      )}
       <p className="text-sm text-muted-foreground">
         {t("logout_state_message")}
       </p>
